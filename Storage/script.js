87909c7bf07cd8f38f98f85d5ef3698cd4b6ebdb
gawhade-tushar <148657863+Tushargawhade@users.noreply.github.com>
2025-09-23 14:23:23 +0530

    Add files via upload

---
+++ Storage/script.js
@@ -0,0 +1,16 @@
+// Local Storage
+
+localStorage.setItem("city" , "Mumbai")
+let city = localStorage.getItem("city")
+
+localStorage.setItem("city1","Delhi")
+let city1 = localStorage.getItem("city1")
+
+localStorage.removeItem("city2")
+localStorage.removeItem("cityq")
+localStorage.removeItem("city1")
+
+localStorage.setItem("city","Delhi")
+
+
+ 
